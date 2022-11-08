c764b4ac65db11a2de3a467a008b0e554fb6ca5a
eiby3 <guilhermemarcondesabe@gmail.com>
2022-11-08 19:21:49 -0300

    Realizando validacoes nas apis.

---
+++ DevFreela.API/Filters/ValidationFilter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace DevFreela.API.Filters
+{
+    public class ValidationFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {            
+            
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            //if (!context.ModelState.IsValid)
+            //{
+            //    var messages = context.ModelState
+            //    .SelectMany(x => x.Value.Errors)
+            //    .Select(x => x.ErrorMessage)
+            //    .ToList();
+
+            //    context.Result = new BadRequestObjectResult(messages);
+            //}
+        }
+    }
+}
