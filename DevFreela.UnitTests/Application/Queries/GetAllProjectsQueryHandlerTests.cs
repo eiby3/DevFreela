4d3df9c300fabc08098dbcb3e93109982c3b9a94
eiby3 <guilhermemarcondesabe@gmail.com>
2022-11-09 21:43:24 -0300

    Criando testes unitarios com mock e o autofixture

---
+++ DevFreela.UnitTests/Application/Queries/GetAllProjectsQueryHandlerTests.cs
@@ -0,0 +1,34 @@
+﻿using AutoFixture;
+using DevFreela.Application.Queries.GetAllProjects;
+using DevFreela.Core.Repositories;
+using Moq;
+
+namespace DevFreela.UnitTests.Application.Queries
+{
+    public class GetAllProjectsQueryHandlerTests
+    {
+        private readonly Fixture _fixture = new Fixture();
+        [Fact]
+        public async Task ManyProjectsExists_Executed_ReturnsProjectViewModels()
+        {
+            //arrange
+            var projectListMock = _fixture.CreateMany<Project>().ToList();
+
+            var projectRepositoryMock = new Mock<IProjectRepository>();
+            projectRepositoryMock.Setup(x => x.GetAllAsync())
+                .Returns(Task.FromResult(projectListMock));
+
+            var getAllProjectsQuery = new GetAllProjectsQuery(It.IsAny<string>());
+            
+            var getAllProjectsQueryHandler = new GetAllProjectsQueryHandler(projectRepositoryMock.Object);
+            //act
+            var projects = await getAllProjectsQueryHandler.Handle(getAllProjectsQuery, new CancellationToken());
+
+            //assert
+            Assert.NotNull(projects);
+            Assert.NotEmpty(projects);
+            Assert.Equal(projectListMock.Count, projects.Count);
+            projectRepositoryMock.Verify(_ => _.GetAllAsync().Result, Times.Once);
+        }
+    }
+}
---
+++ DevFreela.UnitTests/Core/Entities/ProjectTest.cs
@@ -0,0 +1,20 @@
+﻿namespace DevFreela.UnitTests.Core.Entities
+{
+    public class ProjectTest
+    {
+        private readonly Fixture _fixture = new Fixture();
+        [Fact]
+        public void InputProject_Started_ReturnEnumInProgress_And_NotNullStartedAt()
+        {
+            //arrange
+            var project =  _fixture.Create<Project>();
+
+            //act
+            project.Start();
+
+            //assert
+            Assert.Equal(EnumProjectStatus.InProgress, project.Status);
+            Assert.NotNull(project.StartedAt);
+        }
+    }
+}
